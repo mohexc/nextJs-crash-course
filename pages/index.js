b9615d5f9563f5bd47af0dc005d462138705a8f3
nanut <galunyu@gmail.com>
2021-02-26 18:52:04 +0700

    [Added] Header

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 
-export default function Home() {
+export default function Home({ articles }) {
+  console.log('articles', articles)
+
   return (
     <div>
       <Head>
@@ -8,6 +10,23 @@ export default function Home() {
         <meta name="keyword" content="web development, programming" />
       </Head>
       <h1>Welcome to Next</h1>
+      {articles.map(article => (
+        <>
+          <h3>{article.title}</h3>
+          <p>{article.body}</p>
+        </>
+      ))}
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const articles = await res.json()
+
+  return {
+    props: {
+      articles
+    }
+  }
 }
